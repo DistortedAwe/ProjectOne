6e1fdbd4339a7b01194c9763db7832905bbae177
Distorted Awe <distortedawe@gmail.com>
2022-08-17 21:35:34 +1200

    add one input functionality

--- src/script.js
+++ src/script.js
@@ -1,7 +1,16 @@
+function outputOneItem() {
+    var oneInput = document.getElementById("oneInput");
+    var oneInputOutput = "";
+    oneInputOutput =  oneInput.value;
+    document.getElementById("oneInputOutput").innerHTML = oneInputOutput;
+
+
+}
+
 function outputTwoItems() {
     var x = document.getElementById("input");
     var text = "";
     var i;
     text = "&lt;h1&gt;" + x.elements[0].value + " " + x.elements[1].value + "&lt;/h1&gt;";
-    document.getElementById("output").innerHTML = text;
+    document.getElementById("outputTwoItems").innerHTML = text;
 }
